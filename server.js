31410c293896ee5a8c00a11826251e02526338b8
Patrick <porchewellness@gmail.com>
2017-06-19 11:20:18 -0700

    sign pages complete

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get('/', (request, response) => {
   })
 })
 
+app.get('/signup', (request, response) => {
+	response.render('signup')
+})
+
+app.get('/signin', (request, response) => {
+	response.render('signin')
+})
+
 app.get('/albums/:albumID', (request, response) => {
   const albumID = request.params.albumID
 
